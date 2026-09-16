aa18dfaae407e85b40fb9dfbe55b395904e7b105
Nanga Sinclair <nangasinclair81@gmail.com>
2026-09-16 18:37:30 +0300

    add footer componets

---
+++ includes/footer.php
@@ -0,0 +1,38 @@
+</main>
+
+<footer class="mmtc-footer">
+  <div class="container">
+    <div class="row gy-4">
+      <div class="col-md-5">
+        <div class="footer-brand">
+          <img src="images/mmtc-logo.jpeg" alt="MMTC logo" class="brand-logo">
+          <span class="brand-text">MMTC <span class="brand-text-light">Events</span></span>
+        </div>
+        <p class="footer-tagline">Macmillan Medical Training College, Nairobi &mdash; training tomorrow's healthcare workers, today.</p>
+      </div>
+      <div class="col-md-3">
+        <h6 class="footer-heading">Quick links</h6>
+        <ul class="footer-links">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="events.php">Browse events</a></li>
+          <li><a href="records.php">Registration records</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <h6 class="footer-heading">Student Affairs Office</h6>
+        <ul class="footer-links">
+          <li><i class="fa-solid fa-location-dot"></i> Off Waiyaki Way, Nairobi</li>
+          <li><i class="fa-solid fa-envelope"></i> events@mmtc.ac.ke</li>
+          <li><i class="fa-solid fa-phone"></i> +254 700 000 000</li>
+        </ul>
+      </div>
+    </div>
+    <hr class="footer-rule">
+    <p class="footer-copy">&copy; <?php echo date('Y'); ?> Macmillan Medical Training College. Student Event Registration Management System &mdash; Mini Project.</p>
+  </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
